7b691338eef284330f7495724300d93c97f9867c
Zeenobit <zeenobit.exe@gmail.com>
2023-07-10 20:44:47 -0400

    Remove `FromLoaded`

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,8 @@ mod utils;
 /// Common elements for saving/loading world state.
 pub mod prelude {
     pub use crate::load::{
-        component_from_loaded, load_from_file, load_from_file_on_request, Error as LoadError,
-        FromLoaded, LoadFromFileRequest, LoadPlugin, LoadSet, Loaded, Unload,
-        {insert_after_load_with, insert_clone_after_load, insert_default_after_load},
+        load_from_file, load_from_file_on_request, Error as LoadError, LoadFromFileRequest,
+        LoadPlugin, LoadSet, Loaded, Unload,
     };
     pub use crate::save::{
         save_into_file, save_into_file_on_request, Error as SaveError, Save, SaveIntoFileRequest,
