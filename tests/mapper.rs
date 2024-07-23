60d761fb916c2b98d347f9df716773640f46ded8
Zeenobit <zeenobit.exe@gmail.com>
2024-07-22 21:37:28 -0400

    Add full mapper test

---
+++ tests/mapper.rs
@@ -0,0 +1,107 @@
+use bevy::prelude::*;
+use moonshine_save::{load::load_from_file_with_mapper, prelude::*, save::SceneMapper};
+
+const SAVE_PATH: &str = "test_mapper.ron";
+
+#[derive(Bundle)]
+struct FooBundle {
+    foo: Foo,
+    bar: Bar,
+    save: Save,
+}
+
+impl FooBundle {
+    fn new(secret: u32) -> Self {
+        Self {
+            foo: Foo(Box::new(secret)),
+            bar: Bar,
+            save: Save,
+        }
+    }
+}
+
+#[derive(Component)]
+struct Foo(Box<dyn Secret>); // Not serializable
+
+#[derive(Component, Default, Reflect)]
+#[reflect(Component)]
+struct SerializedFoo(u32);
+
+#[derive(Component, Default, Reflect)]
+#[reflect(Component)]
+struct Bar;
+
+trait Secret: 'static + Send + Sync {
+    fn secret(&self) -> u32;
+}
+
+impl Secret for u32 {
+    fn secret(&self) -> u32 {
+        *self
+    }
+}
+
+fn app() -> App {
+    let mut app = App::new();
+    app.register_type::<Bar>()
+        .register_type::<SerializedFoo>()
+        .add_plugins(MinimalPlugins)
+        .add_plugins((SavePlugin, LoadPlugin));
+    app
+}
+
+#[test]
+fn it_works() {
+    {
+        let mut app = app();
+        app.add_systems(
+            PreUpdate,
+            save_default()
+                .map_component::<Foo>(|Foo(data): &Foo| SerializedFoo(data.secret()))
+                .into_file(SAVE_PATH),
+        );
+
+        // Spawn some entities
+        let entity = app.world_mut().spawn(FooBundle::new(42)).id();
+
+        app.update();
+
+        // Check pre-conditions
+        let world = app.world_mut();
+        assert_eq!(world.query::<&Foo>().single(world).0.secret(), 42);
+        assert!(world.entity(entity).contains::<Bar>());
+        assert!(world.entity(entity).contains::<Save>());
+        assert!(!world.entity(entity).contains::<SerializedFoo>());
+
+        // Ensure file was written to disk
+        assert!(std::fs::read(SAVE_PATH).is_ok());
+    }
+
+    {
+        let mut app = app();
+        app.add_systems(
+            PreUpdate,
+            load_from_file_with_mapper(
+                SAVE_PATH,
+                SceneMapper::default().map::<SerializedFoo>(|serialized_foo: &SerializedFoo| {
+                    Foo(Box::new(serialized_foo.0))
+                }),
+            ),
+        );
+
+        // Spawn an entity to offset indices
+        app.world_mut().spawn_empty();
+
+        app.update();
+
+        let world = app.world_mut();
+        let entity = world.query_filtered::<Entity, With<Bar>>().single(world);
+
+        assert_eq!(world.query::<&Foo>().single(world).0.secret(), 42);
+        assert!(world.entity(entity).contains::<Bar>());
+        assert!(world.entity(entity).contains::<Save>());
+        assert!(!world.entity(entity).contains::<SerializedFoo>());
+
+        std::fs::remove_file(SAVE_PATH).unwrap();
+    }
+}
